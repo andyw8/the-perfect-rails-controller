cd2a47566c10196f914e60612180edb05cafe9b5
Andy Waite <andyw8@users.noreply.github.com>
2016-03-22 22:24:41 +0000

    Remove all instance usage

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,54 +1,53 @@
 class WidgetsController < ApplicationController
   def index
-    @widgets = Widget.all
-    render 'index', locals: { widgets: @widgets }
+    widgets = Widget.all
+    render 'index', locals: { widgets: widgets }
   end
 
   def show
-    set_widget
-    render 'show', locals: { widget: @widget }
+    widget = find_widget
+    render 'show', locals: { widget: widget }
   end
 
   def new
-    @widget = Widget.new
-    render 'new', locals: { widget: @widget }
+    widget = Widget.new
+    render 'new', locals: { widget: widget }
   end
 
   def edit
-    set_widget
-    render 'edit', locals: { widget: @widget }
+    widget = find_widget
+    render 'edit', locals: { widget: widget }
   end
 
   def create
-    @widget = Widget.new(widget_params)
+    widget = Widget.new(widget_params)
 
-    if @widget.save
-      redirect_to @widget, notice: 'Widget was successfully created.'
+    if widget.save
+      redirect_to widget, notice: 'Widget was successfully created.'
     else
-      render 'new', locals: { widget: @widget }
+      render 'new', locals: { widget: widget }
     end
   end
 
   def update
-    set_widget
+    widget = find_widget
 
-    if @widget.update(widget_params)
-      redirect_to @widget, notice: 'Widget was successfully updated.'
+    if widget.update(widget_params)
+      redirect_to widget, notice: 'Widget was successfully updated.'
     else
-      render 'edit', locals: { widget: @widget }
+      render 'edit', locals: { widget: widget }
     end
   end
 
   def destroy
-    set_widget
-    @widget.destroy
+    find_widget.destroy
     redirect_to widgets_url, notice: 'Widget was successfully destroyed.'
   end
 
   private
 
-  def set_widget
-    @widget = Widget.find(params[:id])
+  def find_widget
+    Widget.find(params[:id])
   end
 
   def widget_params
