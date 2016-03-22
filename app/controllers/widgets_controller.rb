71ab48a34d4b23e9ad6a3f537a282d481462af1c
Andy Waite <andyw8@users.noreply.github.com>
2016-03-22 21:35:37 +0000

    Remove boilerplate comments

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,25 +1,20 @@
 class WidgetsController < ApplicationController
   before_action :set_widget, only: [:show, :edit, :update, :destroy]
 
-  # GET /widgets
   def index
     @widgets = Widget.all
   end
 
-  # GET /widgets/1
   def show
   end
 
-  # GET /widgets/new
   def new
     @widget = Widget.new
   end
 
-  # GET /widgets/1/edit
   def edit
   end
 
-  # POST /widgets
   def create
     @widget = Widget.new(widget_params)
 
@@ -30,7 +25,6 @@ class WidgetsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /widgets/1
   def update
     if @widget.update(widget_params)
       redirect_to @widget, notice: 'Widget was successfully updated.'
@@ -39,19 +33,16 @@ class WidgetsController < ApplicationController
     end
   end
 
-  # DELETE /widgets/1
   def destroy
     @widget.destroy
     redirect_to widgets_url, notice: 'Widget was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_widget
       @widget = Widget.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def widget_params
       params.require(:widget).permit(:name)
     end
